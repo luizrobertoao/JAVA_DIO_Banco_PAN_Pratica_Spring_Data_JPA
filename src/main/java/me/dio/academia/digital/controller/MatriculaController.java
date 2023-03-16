028e6399fbb7d6a41c8e25485098d8e22a07fd6c
Luiz Roberto <luizrobertoao29@gmail.com>
2023-03-16 18:22:25 -0300

    Cria MatriculaServiceImpl métodos para consultar todas matriculas, matricular aluno, e suas respectivas rotas.

--- src/main/java/me/dio/academia/digital/controller/MatriculaController.java
+++ src/main/java/me/dio/academia/digital/controller/MatriculaController.java
@@ -1,9 +1,27 @@
 package me.dio.academia.digital.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import me.dio.academia.digital.entity.Matricula;
+import me.dio.academia.digital.entity.form.MatriculaForm;
+import me.dio.academia.digital.service.MatriculaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/matriculas")
 public class MatriculaController {
+
+    @Autowired
+    private MatriculaServiceImpl service;
+
+    @PostMapping
+    public Matricula create(@RequestBody MatriculaForm form) {
+        return service.create(form);
+    }
+
+    @GetMapping
+    public List<Matricula> getAll() {
+       return service.getAll();
+    }
 }
